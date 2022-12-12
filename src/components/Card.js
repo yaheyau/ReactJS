2e51a0c9fcdf28548cf62b9e173821d0cd7985d8
yaheyau <YAheyau@gomel.iba.by>
2022-12-12 05:17:28 +0300

    Task1: added Header, Card components

---
+++ src/components/Card.js
@@ -0,0 +1,11 @@
+function Card() {
+  return (
+    <div className="card">
+      <h2 className="card-title">Title</h2>
+      <hr color="black" size="1"></hr>
+      <p className="card-text">Text</p>
+    </div>
+  );
+}
+
+export default Card;
